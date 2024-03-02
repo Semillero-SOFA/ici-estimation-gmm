323f3f565485be0ce9a8cfc74d58896c133c7ba7
cesara.montoya1 <cesara.montoya1@udea.edu.co>
2024-03-02 12:02:35 -0500

    [Bugfix] Finally replaced filenames with correct extensions
    Signed-off-by: cesara.montoya1 <cesara.montoya1@udea.edu.co>

--- 16GBd/gmm_16GBd_plot_results.py
+++ 16GBd/gmm_16GBd_plot_results.py
@@ -36,13 +36,13 @@ gmm_neurons_avg_results = [
     )
     for neurons in gmm_utils.MAX_NEURONS_LIST
 ]
-neurons_filename = Path(f"{RESULTS_DIR}/gmm_neurons_avg_results.json")
+neurons_json_filename = Path(f"{RESULTS_DIR}/gmm_neurons_avg_results.json")
+neurons_svg_filename = Path(f"{RESULTS_DIR}/gmm_neurons_avg_results.svg")
 sofa.save_json(gmm_neurons_avg_results,
-               neurons_filename)
-neurons_filename = neurons_filename.replace("json", "svg")
+               neurons_json_filename)
 gmm_utils.plot_results(gmm_utils.MAX_NEURONS_LIST,
                        gmm_neurons_avg_results,
-                       neurons_filename,
+                       neurons_svg_filename,
                        "Maximum number of neurons", log=True)
 
 # Layers
@@ -58,13 +58,13 @@ gmm_layers_avg_results = [
     )
     for layers in gmm_utils.LAYERS_NUMBER_LIST
 ]
-layers_filename = Path(f"{RESULTS_DIR}/gmm_layers_avg_results.json")
+layers_json_filename = Path(f"{RESULTS_DIR}/gmm_layers_avg_results.json")
+layers_svg_filename = Path(f"{RESULTS_DIR}/gmm_layers_avg_results.svg")
 sofa.save_json(gmm_layers_avg_results,
-               layers_filename)
-layers_filename = layers_filename.replace("json", "svg")
+               layers_json_filename)
 gmm_utils.plot_results(gmm_utils.LAYERS_NUMBER_LIST,
                        gmm_layers_avg_results,
-                       layers_filename,
+                       layers_svg_filename,
                        "Number of layers", log=False, intx=True)
 
 # OSNR
@@ -80,9 +80,9 @@ gmm_osnr_avg_results = [
     )
     for osnr in gmm_utils.OSNR_LIST
 ]
-osnr_filename = Path(f"{RESULTS_DIR}/gmm_osnr_avg_results.json")
+osnr_json_filename = Path(f"{RESULTS_DIR}/gmm_osnr_avg_results.json")
 sofa.save_json(gmm_osnr_avg_results,
-               osnr_filename)
+               osnr_json_filename)
 
 # Sort models by score
 better_models_df = gmm_utils.get_better_models(
