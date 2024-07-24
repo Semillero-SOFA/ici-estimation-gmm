532dae7bfceddee9af65590f4dd224b08e195cd9
cesara.montoya1 <cesara.montoya1@udea.edu.co>
2024-07-23 19:21:10 -0500

    [Feature] Added features calculation
    Signed-off-by: cesara.montoya1 <cesara.montoya1@udea.edu.co>

--- 32GBd/gmm_32GBd_calc_features.py
+++ 32GBd/gmm_32GBd_calc_features.py
@@ -3,15 +3,22 @@
 
 # Libraries
 import os
+import logging
+
 from collections import defaultdict
 from itertools import product
+from pathlib import Path
 
 import numpy as np
-from pathlib import Path
 import scipy as sp
 
 import sofa
 import gmm_utils
+
+FILENAME = os.path.basename(__file__)[:-3]
+
+# Create a logger for this script
+logger = sofa.setup_logger(FILENAME)
 
 # Globals
 LOCAL_ROOT = sofa.find_root()
@@ -23,28 +30,36 @@ GLOBAL_RESULTS_DIR = f"{GLOBAL_ROOT}/results"
 RESULTS_DIR = f"{GLOBAL_RESULTS_DIR}/gmm_32GBd_regression"
 Path(RESULTS_DIR).mkdir(parents=True, exist_ok=True)
 
-# Special function to read the known data structure
+
+def initialize_histograms():
+    # Return 6 levels deep dictionary
+    return defaultdict(
+        defaultdict(
+            defaultdict(
+                defaultdict(
+                    defaultdict(
+                        defaultdict(list).copy
+                    ).copy
+                ).copy
+            ).copy
+        ).copy
+    )
 
 
 def read_data(folder_rx):
-    data = defaultdict(
-        defaultdict(
-            defaultdict(defaultdict(defaultdict(
-                defaultdict().copy).copy).copy).copy
-        ).copy
-    )
+    data = initialize_histograms()
+
     for dist_pow in os.listdir(folder_rx):
         if not os.path.isdir(os.path.join(folder_rx, dist_pow)):
             continue
-        print(f"reading {dist_pow}")
+        logger.info(f"Reading {dist_pow}")
         for spac in os.listdir(os.path.join(folder_rx, dist_pow)):
-            print(f"reading {dist_pow}/{spac}")
+            logger.info(f"Reading {dist_pow}/{spac}")
             consts = os.listdir(os.path.join(folder_rx, dist_pow, spac))
             for const in consts:
                 if const.endswith("xlsx"):
                     continue
                 song, orth, osnr, spacing, distance, power = const.split("_")
-                # Remove extension
                 power = power.split(".")[0]
                 spacing = spacing.replace("p", ".")
                 osnr = osnr.replace("p", ".")
@@ -54,110 +69,98 @@ def read_data(folder_rx):
     return data
 
 
-def get_histograms(data):
-    histograms_hist = defaultdict(
-        defaultdict(
-            defaultdict(defaultdict(defaultdict(
-                defaultdict(list).copy).copy).copy).copy
-        ).copy
-    )
-    histograms_gmm = defaultdict(
-        defaultdict(
-            defaultdict(defaultdict(defaultdict(
-                defaultdict(list).copy).copy).copy).copy
-        ).copy
-    )
+def calculate_gmm_2d(input_data):
+    gm_kwargs = {
+        "means_init": np.array(list(product([-3, -1, 1, 3], repeat=2))),
+        "n_components": 16,
+        "covariance_type": "full",
+    }
+    return gmm_utils.calculate_gmm(input_data, gm_kwargs)
+
+
+def calculate_3d_histogram(x_ch, bins, limits):
+    return gmm_utils.calculate_3d_histogram(x_ch, bins, limits)
+
+
+def calculate_1d_gmms(x_ch, bins):
+    gm_kwargs = {
+        "means_init": np.array([-3, -1, 1, 3]).reshape(4, 1),
+        "n_components": 4,
+        "covariance_type": "full",
+    }
+    hist_x, hist_y = gmm_utils.calculate_1d_histogram(x_ch.real, bins)
+    input_data = np.repeat(hist_x, hist_y).reshape(-1, 1)
+    gm_i = gmm_utils.calculate_gmm(input_data, gm_kwargs)
+
+    hist_x, hist_y = gmm_utils.calculate_1d_histogram(x_ch.imag, bins)
+    input_data = np.repeat(hist_x, hist_y).reshape(-1, 1)
+    gm_q = gmm_utils.calculate_gmm(input_data, gm_kwargs)
+
+    return gm_i, gm_q
+
+
+def process_channel(x_ch, bins, limits):
+    input_data = np.vstack((x_ch.real, x_ch.imag)).T
+    gm_2d = calculate_gmm_2d(input_data)
+    hist_3d = calculate_3d_histogram(x_ch, bins, limits)
+    gm_i, gm_q = calculate_1d_gmms(x_ch, bins)
+    return gm_2d, hist_3d, gm_i, gm_q
+
+
+def process_channels(histograms_hist, histograms_gmm, distance, power, spacing, osnr, song, orth, X_chs, bins, limits):
+    for x_ch in X_chs:
+        gm_2d, hist_3d, gm_i, gm_q = process_channel(x_ch, bins, limits)
+        histograms_hist[distance][power][spacing][osnr][song][orth].append(
+            hist_3d)
+        histograms_gmm[distance][power][spacing][osnr][song][orth].append([
+                                                                          gm_2d, gm_i, gm_q])
+
+
+def calculate_gmm_and_histograms(data):
+    histograms_hist = initialize_histograms()
+    histograms_gmm = initialize_histograms()
     bins = 128
     limits = [-5, 5]
 
-    for distance in data.keys():
-        for power in data[distance].keys():
-            for spacing in data[distance][power].keys():
-                for osnr in data[distance][power][spacing].keys():
-                    for song in data[distance][power][spacing][osnr].keys():
-                        for orth in data[distance][power][spacing][osnr][song].keys():
-                            print(f"Calculating GMM for: {distance}/{power}/{spacing}/{osnr}/{song}/{orth}")
-                            X_rx = data[distance][power][spacing][osnr][song][orth]
+    for distance, powers in data.items():
+        for power, spacings in powers.items():
+            for spacing, osnrs in spacings.items():
+                for osnr, songs in osnrs.items():
+                    for song, orths in songs.items():
+                        for orth, X_rx in orths.items():
+                            logger.info(f"Calculating GMM for: {distance}/{power}/{spacing}/{osnr}/{song}/{orth}")
                             X_chs = gmm_utils.split(X_rx, 3)
+                            process_channels(histograms_hist, histograms_gmm, distance,
+                                             power, spacing, osnr, song, orth, X_chs, bins, limits)
 
-                            for n, x_ch in enumerate(X_chs):
-                                # Calculate 2D GMM
-                                input_data = np.vstack(
-                                    (x_ch.real, x_ch.imag)).T
-                                gm_kwargs = {
-                                    "means_init": np.array(
-                                        list(product([-3, -1, 1, 3], repeat=2))
-                                    ),
-                                    "n_components": 16,
-                                }
-                                gm_2d = gmm_utils.calculate_gmm(
-                                    input_data, gm_kwargs)
-
-                                # Calculate 3D histogram
-                                hist, x_mesh, y_mesh = gmm_utils.calculate_3d_histogram(
-                                    x_ch, bins, limits)
-
-                                # Save 3D histogram
-                                histograms_hist[distance][power][spacing][osnr][song][orth].append(
-                                    hist)
-
-                                # Calculate I and Q histograms
-                                hist_x, hist_y = gmm_utils.calculate_1d_histogram(
-                                    x_ch.real, bins)
-                                input_data = np.repeat(
-                                    hist_x, hist_y).reshape(-1, 1)
-                                gm_kwargs = {
-                                    "means_init": np.array([-3, -1, 1, 3]).reshape(4, 1),
-                                    "n_components": 4,
-                                }
-                                gm_i = gmm_utils.calculate_gmm(
-                                    input_data, gm_kwargs)
-
-                                # Q
-                                hist_x, hist_y = gmm_utils.calculate_1d_histogram(
-                                    x_ch.imag, bins)
-                                input_data = np.repeat(
-                                    hist_x, hist_y).reshape(-1, 1)
-                                gm_kwargs = {
-                                    "means_init": np.array([-3, -1, 1, 3]).reshape(4, 1),
-                                    "n_components": 4,
-                                }
-                                gm_q = gmm_utils.calculate_gmm(input_data,
-                                                               gm_kwargs)
-
-                                # Save gaussians
-                                histograms_gmm[distance][power][spacing][osnr][song][orth].append([
-                                                                                                  gm_2d, gm_i, gm_q])
-
-    histograms_hist = dict(histograms_hist)
-    histograms_gmm = dict(histograms_gmm)
-    return histograms_hist, histograms_gmm
+    return dict(histograms_hist), dict(histograms_gmm)
 
 
 # Load data
 folder_rx = f"{DATABASE_DIR}/Estimation/32GBd"
 
 # Read received data
-print("Reading data...")
+logger.info("Reading data...")
 data = read_data(folder_rx)
 
 # Try to load histograms
 file_models_hist = f"{RESULTS_DIR}/models32_hist.pkl"
 file_models_gmm = f"{RESULTS_DIR}/models32_gmm.pkl"
 
-print("Trying to load features...")
+logger.info("Trying to load features...")
 models_hist = sofa.joblib_load(file_models_hist)
 models_gmm = sofa.joblib_load(file_models_gmm)
-models_tuple = (
-    None if models_hist is None or models_gmm is None else (
-        models_hist, models_gmm)
-)
+models_tuple = None if models_hist is None or models_gmm is None else (
+    models_hist, models_gmm)
 
-# Calculate histograms
-models_tuple = gmm_utils.calc_once("models_tuple", get_histograms,
-                                   {"data": data})
-models_hist, models_gmm = models_tuple
-sofa.joblib_save(models_hist, file_models_hist)
-sofa.joblib_save(models_gmm, file_models_gmm)
-
-print("Features calculated succesfully")
+# Calculate histograms if not loaded
+if models_tuple is None:
+    logger.info("Calculating histograms and GMMs...")
+    models_tuple = gmm_utils.calc_once(
+        "models_tuple", calculate_gmm_and_histograms, {"data": data})
+    models_hist, models_gmm = models_tuple
+    sofa.joblib_save(models_hist, file_models_hist)
+    sofa.joblib_save(models_gmm, file_models_gmm)
+    logger.info("Features calculated successfully")
+else:
+    logger.info("Features loaded successfully")
